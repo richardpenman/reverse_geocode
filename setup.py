dd8064de7ca23cf9042b05c2315262d377ff0de3
CuriousJames <18385102+CuriousJames@users.noreply.github.com>
2020-07-26 13:06:50 +0100

    Cleaned whitespace and incremented version number

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import os
 from distutils.core import setup
 
+
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
+
 setup(
-    name='reverse_geocode', 
-    version='1.4.1',
+    name='reverse_geocode',
+    version='1.5',
     packages=['reverse_geocode'],
-    package_dir={'reverse_geocode' : '.'}, # look for package contents in current directory
-    package_data={'reverse_geocode' : ['geocode.csv', 'countries.csv']},
+    package_dir={'reverse_geocode': '.'},  # look for package contents in current directory
+    package_data={'reverse_geocode': ['geocode.csv', 'countries.csv']},
     author='Richard Penman',
     author_email='richard.penman@gmail.com',
     description='Reverse geocode the given latitude / longitude',
