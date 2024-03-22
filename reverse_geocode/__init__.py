512a8ae76f844c6b1225b5acd813128f56051a70
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2024-03-22 19:52:21 +0800

    apply pep-8 with black

--- reverse_geocode/__init__.py
+++ reverse_geocode/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import csv, logging, os, sys, zipfile
-if sys.platform == 'win32':
-    csv.field_size_limit(2**31-1)
+import csv
+import logging
+import os
+import sys
+import zipfile
+
+if sys.platform == "win32":
+    csv.field_size_limit(2**31 - 1)
 else:
     csv.field_size_limit(sys.maxsize)
 try:
@@ -12,80 +17,78 @@ except ImportError:
 from scipy.spatial import cKDTree as KDTree
 
 # location of geocode data to download
-GEOCODE_URL = 'http://download.geonames.org/export/dump/cities1000.zip'
-GEOCODE_FILENAME = 'cities1000.txt'
+GEOCODE_URL = "http://download.geonames.org/export/dump/cities1000.zip"
+GEOCODE_FILENAME = "cities1000.txt"
 
 
 def singleton(cls):
-    """Singleton pattern to avoid loading class multiple times
-    """
+    """Singleton pattern to avoid loading class multiple times"""
     instances = {}
 
     def getinstance():
         if cls not in instances:
             instances[cls] = cls()
         return instances[cls]
+
     return getinstance
 
 
 @singleton
 class GeocodeData:
-
-    def __init__(self, geocode_filename='geocode.csv', country_filename='countries.csv'):
+    def __init__(
+        self, geocode_filename="geocode.csv", country_filename="countries.csv"
+    ):
         coordinates, self.__locations = self.__extract(rel_path(geocode_filename))
         self.__tree = KDTree(coordinates)
         self.__load_countries(rel_path(country_filename))
 
     def __load_countries(self, country_filename):
-        """Load a map of country code to name
-        """
+        """Load a map of country code to name"""
         self.__countries = {}
-        with open(country_filename, 'r') as handler:
+        with open(country_filename, "r") as handler:
             for code, name in csv.reader(handler):
                 self.__countries[code] = name
 
     def query(self, coordinates):
-        """Find closest match to this list of coordinates
-        """
+        """Find closest match to this list of coordinates"""
         try:
             distances, indices = self.__tree.query(coordinates, k=1)
         except ValueError as e:
-            logging.info('Unable to parse coordinates: {}'.format(coordinates))
+            logging.info("Unable to parse coordinates: {}".format(coordinates))
             raise e
         else:
             results = [self.__locations[index] for index in indices]
             for result in results:
-                result['country'] = self.__countries.get(result['country_code'], '')
+                result["country"] = self.__countries.get(result["country_code"], "")
             return results
 
     def __download(self):
-        """Download geocode file
-        """
+        """Download geocode file"""
         local_filename = os.path.abspath(os.path.basename(GEOCODE_URL))
         if not os.path.exists(local_filename):
-            logging.info('Downloading: {}'.format(GEOCODE_URL))
+            logging.info("Downloading: {}".format(GEOCODE_URL))
             urlretrieve(GEOCODE_URL, local_filename)
         return local_filename
 
     def __extract(self, local_filename):
-        """Extract geocode data from zip
-        """
+        """Extract geocode data from zip"""
         if os.path.exists(local_filename):
             # open compact CSV
-            rows = csv.reader(open(local_filename, 'r'))
+            rows = csv.reader(open(local_filename, "r"))
         else:
             if not os.path.exists(GEOCODE_FILENAME):
                 # remove GEOCODE_FILENAME to get updated data
                 downloaded_file = self.__download()
-                logging.info('Extracting: {}'.format(GEOCODE_FILENAME))
+                logging.info("Extracting: {}".format(GEOCODE_FILENAME))
                 with zipfile.ZipFile(downloaded_file) as z:
-                    with open(GEOCODE_FILENAME, 'wb') as fp:
+                    with open(GEOCODE_FILENAME, "wb") as fp:
                         fp.write(z.read(GEOCODE_FILENAME))
+                os.remove(downloaded_file)
 
             # extract coordinates into more compact CSV for faster loading
-            writer = csv.writer(open(local_filename, 'w'))
+            writer = csv.writer(open(local_filename, "w"))
             rows = []
-            for row in csv.reader(open(GEOCODE_FILENAME, 'r'), delimiter='\t'):
+            for row in csv.reader(open(GEOCODE_FILENAME, "r"), delimiter="\t"):
                 latitude, longitude = row[4:6]
                 country_code = row[8]
                 if latitude and longitude and country_code:
@@ -94,7 +97,6 @@ class GeocodeData:
                     writer.writerow(row)
                     rows.append(row)
             # cleanup downloaded files
-            os.remove(downloaded_file)
             os.remove(GEOCODE_FILENAME)
 
         # load a list of known coordinates and corresponding __locations
@@ -106,26 +108,23 @@ class GeocodeData:
 
 
 def rel_path(filename):
-    """Return the path of this filename relative to the current script
-    """
+    """Return the path of this filename relative to the current script"""
     return os.path.join(os.getcwd(), os.path.dirname(__file__), filename)
 
 
 def get(coordinate):
-    """Search for closest known location to this coordinate
-    """
+    """Search for closest known location to this coordinate"""
     gd = GeocodeData()
     return gd.query([coordinate])[0]
 
 
 def search(coordinates):
-    """Search for closest known locations to these coordinates
-    """
+    """Search for closest known locations to these coordinates"""
     gd = GeocodeData()
     return gd.query(coordinates)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # test some coordinate lookups
     city1 = -37.81, 144.96
     city2 = 31.76, 35.21
--- setup.py
+++ setup.py
@@ -7,16 +7,29 @@ def read(filename):
 
 
 setup(
-    name='reverse_geocode',
-    version='1.6',
-    packages=['reverse_geocode'],
-    package_dir={'reverse_geocode': '.'},  # look for package contents in current directory
-    package_data={'reverse_geocode': ['geocode.csv', 'countries.csv']},
-    author='Richard Penman',
-    author_email='richard.penman@gmail.com',
-    description='Reverse geocode the given latitude / longitude',
-    long_description=read('README.rst'),
-    url='https://github.com/richardpenman/reverse_geocode/',
-    license='lgpl',
-    install_requires=['numpy', 'scipy']
+    name="reverse_geocode",
+    version="1.6.1",
+    packages=["reverse_geocode"],
+    package_dir={"reverse_geocode": "reverse_geocode"},
+    data_files=[
+        (
+            "reverse_geocode",
+            ["reverse_geocode/geocode.csv", "reverse_geocode/countries.csv"],
+        )
+    ],
+    author="Richard Penman",
+    author_email="richard.penman@gmail.com",
+    description="Reverse geocode the given latitude / longitude",
+    long_description=read("README.rst"),
+    url="https://github.com/richardpenman/reverse_geocode/",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    license="lgpl",
+    install_requires=["numpy", "scipy"],
 )
