5a1e77f0894d3c7135a6908ec21da373b3d502eb
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2024-03-22 19:56:17 +0800

    add unit test

---
+++ test_reverse_geocode.py
@@ -0,0 +1,19 @@
+import reverse_geocode
+import unittest
+
+
+class TestBuiltwith(unittest.TestCase):
+    def test_wordpress(self):
+        coordinates = (-37.81, 144.96), (31.76, 35.21)
+        results = reverse_geocode.search(coordinates)
+        self.assertEqual(
+            results,
+            [
+                {"city": "Melbourne", "country_code": "AU", "country": "Australia"},
+                {"city": "Jerusalem", "country_code": "IL", "country": "Israel"},
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
